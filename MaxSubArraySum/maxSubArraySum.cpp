27e4adefdf6c77924e60124758e9b093ebb5e682
adityag795 <adityag795@gmail.com>
2017-08-08 18:17:03 +0530

    To find Maximum Sub Array Sum

---
+++ MaxSubArraySum/maxSubArraySum.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+#include<vector>
+using namespace std;
+
+int maxSubArraySum(vector<int> &a) 
+{
+	int size = a.size();
+	int maxSoFar = 0, 
+	maxEndingHere = 0;
+	for(int i = 0; i < size; i++)
+	{
+		maxEndingHere = maxEndingHere + a[i];
+		if (maxEndingHere < 0)
+			maxEndingHere = 0;
+		if (maxSoFar < maxEndingHere)
+			maxSoFar = maxEndingHere;
+	}
+	return maxSoFar; 
+}
+
+int main() 
+{
+	vector<int> arr = { 1,-2,3,4,-4,6,-14,8,2 };
+	cout << "Max sub array sum :" << maxSubArraySum(arr); 
+}
