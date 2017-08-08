89d77246a89b0b662cddd561fc9be8be291e27c2
Aditya Gupta <adityag795@gmail.com>
2017-08-08 07:47:43 +0530

    To demonstrate ways of passing parameters in C++

---
+++ Parameter_Passing/ParameterPassing.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+void incrementPassByValue(int var)
+{
+    var++;
+}
+
+void incrementPassByReference(int &var)
+{
+    var++;
+}
+
+void incrementPassByPointer(int *ptr) 
+{
+    (*ptr)++;
+}
+
+int main()
+{
+    int i = 10;
+    int ch;
+    cout << "1. Pass by Value.\n";
+    cout << "2. Pass by Reference.\n";
+    cout << "3. Call by Pointer\n";
+    cout << "Enter your choice: ";
+    cin>>ch;
+
+    switch (ch)
+    {
+        case 1:
+            cout << "\nValue of i before increment is :" << i << endl;
+            incrementPassByValue(i);
+            cout << "\nValue of i after increment is :" << i << endl;
+            break;
+        case 2:
+            cout << "\nValue of i before increment is :" << i << endl;
+            incrementPassByReference(i);
+            cout << "\nValue of i after increment is :" << i << endl;
+            break;
+        case 3:
+            cout << "\nValue of i before increment is :" << i << endl;
+            incrementPassByPointer(&i);
+            cout << "\nValue of i after increment is :" << i << endl;
+            break;
+        default:cout<<"\nEnter right choice(1,2,3).";
+    }
+}
